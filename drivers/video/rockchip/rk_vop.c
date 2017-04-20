ae06287c0af3fd4fb26190456e960dcd94d9fe7e
eric.gao@rock-chips.com <eric.gao@rock-chips.com>
2017-04-20 12:45:53 +0800

    rockchip: video: vop: Fix rk_display_init() return error
    The function clk_set_rate() will return it's input parameter, so it's return
    value in normal condition is nonzero. In this case, we should report error
    when it return zero rather than return a nonzero value.
    
    Signed-off-by: Eric Gao <eric.gao@rock-chips.com>

--- drivers/video/rockchip/rk_vop.c
+++ drivers/video/rockchip/rk_vop.c
@@ -242,11 +242,13 @@ int rk_display_init(struct udevice *dev, ulong fbbase,
 	}
 
 	ret = clk_get_by_index(dev, 1, &clk);
-	if (!ret)
+	if (!ret) {
 		ret = clk_set_rate(&clk, timing.pixelclock.typ);
-	if (ret) {
-		debug("%s: Failed to set pixel clock: ret=%d\n", __func__, ret);
-		return ret;
+		if (!ret) {
+			debug("%s: Failed to set pixel clock: ret=%d\n",
+			      __func__, ret);
+			return ret;
+		}
 	}
 
 	rkvop_mode_set(regs, &timing, vop_id);
